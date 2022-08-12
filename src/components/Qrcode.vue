91b344d3420973a5850ca3bb76ea4772ce9302bf
shitingbao <871258317@qq.com>
2022-08-12 18:08:31 +0800

    perf: qrcode

---
+++ src/components/Qrcode.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    二维码解码
+    <el-button class="login-table-signin" type="primary" @click="send">
+      ping
+    </el-button>
+  </div>
+</template>
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+
+function send() {
+  // this.$socket.io.emit("ping", "ping");
+  console.log("ping:", "ping");
+}
+
+onMounted(() => {
+  console.log("onMounted 123");
+});
+</script>
